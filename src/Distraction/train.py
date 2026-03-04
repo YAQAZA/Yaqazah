194f477be44bb35e40238990d0795f47f7e2ac7c
yousef-showman <yousefshowman20@gmail.com>
2026-03-04 23:37:06 +0200

    to train yolo

--- src/Distraction/train.py
+++ src/Distraction/train.py
@@ -1,35 +1,3 @@
-# from roboflow import Roboflow
-#
-# rf = Roboflow(api_key="")
-#
-# # Dataset 1 — Smoker YOLO (already downloaded ✅)
-# project = rf.workspace("cigaretteple-7m0hn").project("smoker-yolo")
-# dataset = project.version(2).download("yolov11")
-#
-# # Dataset 2 — Fix: use version 2 instead of 1
-# project2 = rf.workspace("yolo-dataset-rtznj").project("smoking-and-drinking-detection")
-# dataset2 = project2.version(2).download("yolov11")
-#
-# # Dataset 3 — Driver specific: phone + smoking (bonus, good for your GP)
-# project3 = rf.workspace("ai-czjl4").project("driver-phone-smoking")
-# dataset3 = project3.version(1).download("yolov11")
-
-
-# see the dataset structure##########################################################3
-# import yaml
-#
-# for dataset_path in [
-#     "Smoker-YOLO-2/data.yaml",
-#     "Smoking-and-Drinking-Detection-2/data.yaml"
-# ]:
-#     with open(dataset_path) as f:
-#         data = yaml.safe_load(f)
-#     print(f"\n{dataset_path}")
-#     print(f"Classes: {data['names']}")
-##################################################################################
-
-#train script
-
 from ultralytics import YOLO
 
 model = YOLO("yolo11n.pt")

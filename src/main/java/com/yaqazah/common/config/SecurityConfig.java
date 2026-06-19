90786f6eb2698473cbf468ab030e4ac37d01b4be
Muhammad Hasan <muhammadhasanfarouk@gmail.com>
2026-06-20 01:30:46 +0300

    Trying upstash

--- src/main/java/com/yaqazah/common/config/SecurityConfig.java
+++ src/main/java/com/yaqazah/common/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                                         "/swagger-resources/**",
                                         "/webjars/**"
                                                 ,"/api/files/**"
+                                        ,"/api/redis-test/**"
 //                                        ,"/api/test/**"
 //                                ,"/api/detections/**"
                                 ).permitAll()

3c26aefb7934f5b073abd7542a0dc95a87cc9da2
Muhammad Hasan <muhammadhasanfarouk@gmail.com>
2026-06-20 07:04:11 +0300

    Update YaqazahApplicationTests.java

--- src/test/java/com/yaqazah/YaqazahApplicationTests.java
+++ src/test/java/com/yaqazah/YaqazahApplicationTests.java
@@ -1,5 +1,7 @@
 package com.yaqazah;
 
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import com.azure.storage.blob.BlobServiceClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -16,6 +18,10 @@ class YaqazahApplicationTests {
     // 2. Magically wire the URL, Username, and Password into Spring Boot
     @ServiceConnection
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    // This tells Spring Boot: "Fake the Azure connection during tests so it doesn't crash"
+    @MockitoBean
+    private BlobServiceClient blobServiceClient;
 
     @Test
     void contextLoads() {
